1f0008298182f99cad0b9ffc28b646f2801d06c0
yi0709-fgo <15071248053@163.com>
2025-07-03 14:38:12 +0200

    practise

---
+++ src/main/java/com/example/Codility/Fish.java
@@ -0,0 +1,67 @@
+package com.example.Codility;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Fish {
+    private static String CODING_URL = "https://app.codility.com/programmers/lessons/7-stacks_and_queues/fish/";
+    public static void main(String[] args) {
+
+        int[] A = {4,3,2,1,1,0};
+        int[] B = {0,0,0,1,0,0};
+
+        System.out.println(fish2(A,B));
+
+    }
+    public static int fish2(int[] A, int[] B){
+        ArrayDeque<Integer> downstreams = new ArrayDeque<>();
+        int alive = 0;
+        for (int i = 0; i < A.length; i++) {
+            int currFish = A[i] * (B[i] == 1 ? -1 : 1);
+            if (currFish < 0) {
+                downstreams.push(currFish);
+                alive++;
+            } else {
+                Iterator it = downstreams.iterator();
+                boolean eaten = false;
+                while (it.hasNext()) {
+                    int down = (int) it.next();
+                    if (Math.abs(currFish) > Math.abs(down)) {
+                        it.remove();
+                        alive--;
+                        eaten = false;
+                    } else {
+                        eaten = true;
+                        break;
+                    }
+                }
+                if (!eaten) {
+                    alive++;
+                }
+            }
+        }
+        return alive;
+    }
+
+    public static int fish(int[] A, int[] B) {
+        // Implement your solution here
+        Stack<Integer> pool = new Stack<Integer>();
+        pool.push(0);
+        for(int i=1; i<A.length; i++){
+            int index = pool.peek();
+
+            if(B[index]==1 && B[index]!=B[i]){
+                if(A[index]>A[i]){
+                    continue;
+                }else{
+                    pool.pop();
+                    pool.push(i);
+                }
+            }else{
+                pool.push(i);
+            }
+        }
+        return pool.size();
+    }
+}
